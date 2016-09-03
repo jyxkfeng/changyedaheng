9ac202c07c08749f7c7a6d36a9dccda53516a8c4
jyxkfeng <403714121@qq.com>
2016-09-04 00:01:16 +0800

    9.2
    上传并图片 完成单子

--- js/directive.js
+++ js/directive.js
@@ -1,7 +1,7 @@
-(function(window,angular){
+(function(window, angular) {
 	angular.module('app')
 		//一级header
-		.directive('footerDirective', ['$state','$rootScope', function($state,$rootScope){
+		.directive('footerDirective', ['$state', '$rootScope', function($state, $rootScope) {
 			// Runs during compile
 			return {
 				// name: '',
@@ -17,35 +17,33 @@
 				// transclude: true,
 				// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 				link: function($scope, iElm, iAttrs, controller) {
-						$scope.gohome=function(){
-							console.log('index');
-							$rootScope.menuindex=1;
-							$state.go('index');
-							
-							
-							
-						}
-						$scope.gogame=function(){
-							console.log('gamelistCtrl');
-							$rootScope.menuindex=2;
-							$state.go('gamelist');
-						}
-						$scope.gokf=function(){
-							console.log('kf');
-							$rootScope.menuindex=3;
-							$state.go('kf');
-						}
-						$scope.gomy=function(){
+					$scope.gohome = function() {
+						console.log('index');
+						$rootScope.menuindex = 1;
+						$state.go('index');
+
+					}
+					$scope.gogame = function() {
+						console.log('gamelistCtrl');
+						$rootScope.menuindex = 2;
+						$state.go('gamelist');
+					}
+					$scope.gokf = function() {
+						console.log('kf');
+						$rootScope.menuindex = 3;
+						$state.go('kf');
+					}
+					$scope.gomy = function() {
 							console.log('myhome');
-							$rootScope.menuindex=4;
+							$rootScope.menuindex = 4;
 							$state.go('myhome');
 						}
-					// console.log($scope.stateName);
+						// console.log($scope.stateName);
 				}
 			};
 		}])
 		//二级header
-		.directive('subHeaderDirctive', ['$state','$stateParams', function($state,$stateParams){
+		.directive('subHeaderDirctive', ['$state', '$stateParams', function($state, $stateParams) {
 			// Runs during compile
 			return {
 				// name: '',
@@ -62,31 +60,31 @@
 				// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 				link: function($scope, iElm, iAttrs, controller) {
 					// console.log($stateParams.source);
-					if($stateParams.source == 'app'){
+					if($stateParams.source == 'app') {
 						$scope.subHeaderShow = false;
 						console.log(1);
-					}else{
+					} else {
 						$scope.subHeaderShow = true;
 					}
 
 					// console.log($state.current.goHomeShow);
-					if(angular.isUndefined($state.current.goHomeShow)){
+					if(angular.isUndefined($state.current.goHomeShow)) {
 						$scope.goIndexShow = true;
-					}else if($state.current.goHomeShow == false){
+					} else if($state.current.goHomeShow == false) {
 						$scope.goIndexShow = false;
 					}
-					
-					$scope.goBack = function(){
-							history.go(-1);
+
+					$scope.goBack = function() {
+						history.go(-1);
 					}
-					
+
 					// if($stateParams.source == 'app'){
 
 					// }
 				}
 			};
 		}])
-		.directive('kfboxDirective', [function(){
+		.directive('kfboxDirective', [function() {
 			// Runs during compile
 			return {
 				// name: '',
@@ -102,20 +100,20 @@
 				// transclude: true,
 				// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 				link: function($scope, iElm, iAttrs, controller) {
-					$scope.tiphide=function(){
-						$scope.tipShow=false;
+					$scope.tiphide = function() {
+						$scope.tipShow = false;
 						console.log($scope.tipShow);
 					}
 				}
 			};
 		}])
-		.directive('uploadpicDirective', ['$state','$http','lyer',function($state,$http,lyer){
+		.directive('uploadpicDirective', ['$state', '$http','API', 'lyer','playInfo', function($state, $http,API, lyer,playInfo) {
 			// Runs during compile
 			return {
 				// name: '',
 				// priority: 1,
 				// terminal: true,
-				 //scope:true, // {} = isolate, true = child, false/undefined = no change
+				//scope:true, // {} = isolate, true = child, false/undefined = no change
 				// controller: function($scope, $element, $attrs, $transclude) {},
 				// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 				restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
@@ -125,87 +123,86 @@
 				// transclude: true,
 				// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 				link: function($scope, iElm, iAttrs, controller) {
-					$scope.img=$scope.playInfo.PicUrl;
-					$scope.UpLoadPic=function(){
-						document.querySelector('#localfile').click();
-					}
-					$scope.fileNameChanged=function(sender){
-						$scope.img=document.querySelector('#localfile').value;
-						$scope.upFile=document.querySelector('#localfile').value;
-						var objPreview = document.getElementById('img_src'); 
-						//document.getElementById('img_submit').click();
-						
-						if( !sender.value.match( /.jpg|.jpeg|.gif|.png|.bmp/i ) ){ 
-								console.log('图片格式无效！'); 
-								return false; 
+					    $scope.ablebtn=false;
+						$scope.UpLoadPic = function() {
+							document.querySelector('#localfile').click();
 						}
-						if( sender.files && sender.files[0] ){ //这里面就是chrome和ff可以兼容的了 
-							// Firefox 因安全性问题已无法直接通过 input[file].value 获取完整的文件路径 
-							objPreview.src = window.URL.createObjectURL(sender.files[0]);
-							//objPreview.src = sender.files[0].getAsDataURL();
-							//document.querySelector('#schemeid').value=$scope.playInfo.SchemeId;
-							//document.querySelector('#img_submit').click();
-							 var params = {
-									userfile:sender.files[0]
+						$scope.fileNameChanged = function(sender) {
+							if(!sender.value.match(/.jpg|.jpeg|.gif|.png|.bmp/i)) {
+								console.log('图片格式无效！');
+								return false;
+							}
+							if(sender.files && sender.files[0]) { //这里面就是chrome和ff可以兼容的了 
+								var params = {
+									userfile: sender.files[0]
 								}
-						$http({
-				            method: 'POST',
-				            url: 'http://api.cydhch.com/api/UpLoadFile/UpLoadPic/',
-				            headers: {
-				                'Content-Type':undefined
-				            },
-				            data: params,
-				            transformRequest: function (data, headersGetter) {
-				                var formData = new FormData();
-				                angular.forEach(data, function (value, key) {
-				                    formData.append(key, value);
-				                });
-				
-				                var headers = headersGetter();
-				                delete headers['Content-Type'];
-				
-				                return formData;
-				            }
-				        })
-							// $http.post('http://api.cydhch.com/api/UpLoadFile/UpLoadPic/',params)
-								.success(function(rt) {
+								$http({
+										method: 'POST',
+										url: 'http://api.cydhch.com/api/UpLoadFile/UpLoadPic/',
+										headers: {
+											'Content-Type': undefined
+										},
+										data: params,
+										transformRequest: function(data, headersGetter) {
+											var formData = new FormData();
+											angular.forEach(data, function(value, key) {
+												formData.append(key, value);
+											});
+
+											var headers = headersGetter();
+											delete headers['Content-Type'];
+
+											return formData;
+										}
+									})
+									.success(function(rt) {
 										rt = angular.fromJson(rt)
 										if(rt.Code == 0) {
 											console.log(rt);
-											lyer.msg(rt.Msg,function(){
-												$state.reload()
-											});					
+											lyer.msg(rt.Msg, function() {
+												$scope.pic = 'http://api.cydhch.com/Pic/Payment/temp/' + rt.Data;
+												
+												 $scope.ablebtn=true;
+												//$state.reload()
+											});
 										} else {
 											lyer.msg(rt.Msg);
 											return false;
 										}
 									});
-//							 	API.ptInt('/api/UpLoadFile/UpLoadPic/', params)
-//									.success(function(rt) {
-//										rt = angular.fromJson(rt)
-//										if(rt.Code == 0) {
-//											console.log(rt);
-//											lyer.msg(rt.Msg,function(){
-//												$state.reload()
-//											});					
-//										} else {
-//											lyer.msg(rt.Msg);
-//											return false;
-//										}
-//									});
+							}
 						}
+								//付款
+							$scope.PlayPay = function() {
+								var params = {
+									schemeid: playInfo.get()[0].SchemeId,
+									pic:$scope.pic
+								}
+								//确认无误,付款订单
+								//确认无误,创建订单
+								API.qtInt('/api/yqsPlay/PlayPay/', params)
+									.success(function(rt) {
+										rt = angular.fromJson(rt)
+										if(rt.Code == 0) {
+											console.log(rt);
+											$state.reload()
+					
+										} else {
+											lyer.msg(rt.Msg);
+											return false;
+										}
+									});
+							
+							}
 						
-						
-						
-						//objPreview.filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src = imgSrc; 
 						
 					}
-					
-				}
+			
+
 			};
 		}])
 		//购物车
-		.directive('cartDirective', [function(){
+		.directive('cartDirective', [function() {
 			// Runs during compile
 			return {
 				// name: '',
@@ -221,12 +218,12 @@
 				// transclude: true,
 				// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 				link: function($scope, iElm, iAttrs, controller) {
-					
+
 				}
 			};
 		}])
 		//购物车
-		.directive('loadingDirective', [function(){
+		.directive('loadingDirective', [function() {
 			// Runs during compile
 			return {
 				// name: '',
@@ -248,5 +245,5 @@
 				}
 			};
 		}]);
-		
-})(window,angular);
+
+})(window, angular);
